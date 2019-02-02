11246713f231f8eeea825ca5741e8a43e3ebc2cf
Tomasz Grochowski <tg@urias.it>
2019-02-02 13:30:27 +0100

    car hit ragdoll fix

--- Assets/CharacterPackage/Scripts/HumanoidRagdollBehaviour.cs
+++ Assets/CharacterPackage/Scripts/HumanoidRagdollBehaviour.cs
@@ -22,6 +22,7 @@ namespace CharacterPackage.Scripts
 
 		public void DoRagdoll(bool isRagdoll)
 		{
+			if(isRagdoll)
 			Debug.Log("doing ragdoll");
 
 			foreach (var col in allColliders)
@@ -31,15 +32,9 @@ namespace CharacterPackage.Scripts
 
 			mainCollider.enabled = !isRagdoll;
 			rb.useGravity = !isRagdoll;
+			rb.isKinematic = isRagdoll;
 			anim.enabled = !isRagdoll;
 		}
-
-
-		private void OnTriggerEnter(Collider other)
-		{
-			throw new System.NotImplementedException();
-		}
-
 
 		void OnCollisionEnter (Collision collision) {
 			// are is the collider heading in the direction of the person 0.5 is arbitrary, it may require more or less
@@ -54,39 +49,11 @@ namespace CharacterPackage.Scripts
 			Debug.Log(Vector3.Dot(colRigidbody.velocity, transform.position));
 			
 			if (Vector3.Dot(colRigidbody.velocity, transform.position) < 1000) return;
-			// create a hit and see how close the actual hit point is.
-			
-			Debug.DrawRay(transform.position, col.transform.position, Color.green,2);
-			Debug.Log("Car hit");
-			
-			RaycastHit hit;
-			if (!Physics.Linecast(transform.position, col.transform.position, out hit)) return;
-			Debug.Log("Linecast hit");
-			// are we less than 1 frame from hitting the person?
-				
-			Debug.Log(colRigidbody.velocity.magnitude);
-			Debug.Log(Vector3.Distance(transform.position, hit.point));
 
-			if(colRigidbody.velocity.magnitude * Time.deltaTime / Vector3.Distance(transform.position, hit.point)<1){
-				//We are about to hit...
-				DoRagdoll(true);
-				transform.position += new Vector3(0f, 0.05f, 0f);
-//                    Rigidbody bone = FallDown ();
-//                    bone.AddForce((colRigidbody.velocity.normalized * fallForce) + new Vector3(0f, fallForce, 0f), ForceMode.Impulse);
-                    
-			}
-		}
-        
-		Rigidbody FallDown()
-		{
-			anim.enabled = false;		
-//            GameObject rag = (GameObject)Instantiate(ragdoll, transform.position, transform.rotation);
-//            transform.position = new Vector3(0, -300, 0);
-//            Rigidbody bone = rag.GetComponentInChildren<Rigidbody>();
-//            m_Camera.FocusOn(bone);
-			//timeToGetUp = Time.time + timeToStayDown;		
-//            return bone;
-			return null;
+			var force = colRigidbody.velocity.magnitude;
+			Vector3 dir = collision.contacts[0].point;
+			GetComponent<Rigidbody>().AddForce(dir.normalized*force);
+			DoRagdoll(true);
 		}
 	}
 }
